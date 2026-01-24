f6d24b4a3dd61051001e8cc401d27e1a96769e6f
David <114586818+ZDavid73@users.noreply.github.com>
2026-01-23 23:30:29 -0500

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { FeaturedProducts } from "@/components/products/FeaturedProducts";
+import { Hero } from "@/components/Hero";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <FeaturedProducts />
+    </>
+  );
+}
