359a0073fd0904441f8f05ea936d19395864faa0
David <114586818+ZDavid73@users.noreply.github.com>
2026-02-04 00:42:50 -0500

    Agregar TCG search y correcciones de fetch

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import '@radix-ui/themes/styles.css';
 import { Theme } from '@radix-ui/themes';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 
 import Landing from './src/pages/Landing';
 import About from './src/pages/About';
